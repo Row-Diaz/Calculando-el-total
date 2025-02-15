b33e8cc90117eed2c789e1810d6c1486686e782b
ROW <axlrower@gmail.com>
2025-02-14 22:45:42 -0300

    manipulacion del MOD botones + y -

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,29 @@
+let resta = document.querySelector("#restar")
+let suma = document.getElementById("sumar")
+
+let cantidad = 0
+let cantidaddisplay = document.querySelector("span.cantidad")
+cantidaddisplay.innerText = cantidad
+
 precio = 400000
+
+let total = document.querySelector("#total")
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+function incrementar(){
+    cantidad = cantidad + 1
+    cantidaddisplay.innerText = cantidad
+    total.innerText = cantidad * precio
+}
+
+
+const restar = function() {
+    if (cantidad>0) {
+    cantidad = cantidad - 1
+    cantidaddisplay.innerText = cantidad
+    total.innerText = cantidad * precio
+    actualizarValores()
+}
+}
