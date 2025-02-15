4d5f5f6368e94835bc462cfd53561b85289ef6e7
ROW <axlrower@gmail.com>
2025-02-14 22:58:20 -0300

    inclusion de imagen laptop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,28 @@
-let resta = document.querySelector("#restar")
-let suma = document.getElementById("sumar")
+let resta = document.querySelector("#restar");
+let suma = document.getElementById("sumar");
 
-let cantidad = 0
-let cantidaddisplay = document.querySelector("span.cantidad")
-cantidaddisplay.innerText = cantidad
+let cantidad = 0;
+let cantidaddisplay = document.querySelector("span.cantidad");
+cantidaddisplay.innerText = cantidad;
 
-precio = 400000
+precio = 400000;
 
-let total = document.querySelector("#total")
+let total = document.querySelector("#total");
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
 
-function incrementar(){
-    cantidad = cantidad + 1
-    cantidaddisplay.innerText = cantidad
-    total.innerText = cantidad * precio
+function incrementar() {
+  cantidad = cantidad + 1;
+  cantidaddisplay.innerText = cantidad;
+  total.innerText = cantidad * precio;
 }
 
-
-const restar = function() {
-    if (cantidad>0) {
-    cantidad = cantidad - 1
-    cantidaddisplay.innerText = cantidad
-    total.innerText = cantidad * precio
-    actualizarValores()
-}
-}
+const restar = function () {
+  if (cantidad > 0) {
+    cantidad = cantidad - 1;
+    cantidaddisplay.innerText = cantidad;
+    total.innerText = cantidad * precio;
+    actualizarValores();
+  }
+};
